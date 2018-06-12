27f833a66bbb1e1c8f65a23778cd0723d0e717ee
Gantcho Kojuharov <gantcho@greyarch.net>
2018-06-12 12:01:11 +0200

    using ip package instead of request-ip

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const requestIp = require('request-ip');
+const ip = require('ip');
 const {send} = require('micro')
 
 module.exports = (req, res) => {
-  const ip = requestIp.getClientIp(req)
-  send(res, 200, {ip: ip})
+  send(res, 200, {ip: ip.address()})
 }

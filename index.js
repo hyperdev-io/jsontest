07e5641ffe423e8d2956c5cd118c33ea735711ab
Gantcho Kojuharov <gantcho@greyarch.net>
2018-06-12 13:42:10 +0200

    do not use npm packages for the client ip

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const ip = require('ip');
 const {send} = require('micro')
 
 module.exports = (req, res) => {
-  send(res, 200, {ip: ip.address()})
+  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress
+  send(res, 200, {ip: ip})
 }
